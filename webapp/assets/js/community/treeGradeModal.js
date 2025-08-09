cbfddb24bd860f75c990364c8bbae992790347c9
uptomaster <lnh929@naver.com>
2025-08-10 01:19:53 +0900

    250810 01:19 이남혁 lnh브랜치에서 작업중

--- webapp/assets/js/community/treeGradeModal.js
+++ webapp/assets/js/community/treeGradeModal.js
@@ -6,37 +6,37 @@ document.addEventListener('DOMContentLoaded', () => {
   const closeBtn = document.getElementById('closeTreeInfoModal');
 
   const treeData = {
-  '씨앗': {
-    img: './../../assets/img/씨앗.png',
-    name: '씨앗 등급',
-    description: '새로운 시작을 의미하며, 가능성과 잠재력을 상징합니다.'
-  },
-  '새싹': {
-    img: './../../assets/img/새싹.png',
-    name: '새싹 등급',
-    description: '성장의 초기 단계로, 꾸준한 노력을 통해 앞으로 나아가는 단계입니다.'
-  },
-  '잎새': {
-    img: './../../assets/img/잎새.png',
-    name: '잎새 등급',
-    description: '자연을 상징하며 생명의 힘을 나타냅니다. 환경 보호에 기여한 분들에게 주어집니다.'
-  },
-  '가지': {
-    img: './../../assets/img/가지.png',
-    name: '가지 등급',
-    description: '성장과 확장의 상징으로, 공동체에 활력을 더하는 역할을 합니다.'
-  },
-  '나무': {
-    img: './../../assets/img/나무.png',
-    name: '나무 등급',
-    description: '튼튼하고 성실한 나무처럼 꾸준히 성장하는 등급입니다.'
-  },
-  '관리자': {
-    img: './../../assets/img/관리자.png',
-    name: '관리자 등급',
-    description: '커뮤니티를 관리하며 전체 운영을 책임지는 특별한 등급입니다.'
-  }
-};
+    '씨앗': {
+      img: './../../assets/img/씨앗.png',
+      name: '씨앗 등급',
+      description: '새로운 시작을 의미하며, 가능성과 잠재력을 상징합니다.'
+    },
+    '새싹': {
+      img: './../../assets/img/새싹.png',
+      name: '새싹 등급',
+      description: '성장의 초기 단계로, 꾸준한 노력을 통해 앞으로 나아가는 단계입니다.'
+    },
+    '잎새': {
+      img: './../../assets/img/잎새.png',
+      name: '잎새 등급',
+      description: '자연을 상징하며 생명의 힘을 나타냅니다. 환경 보호에 기여한 분들에게 주어집니다.'
+    },
+    '가지': {
+      img: './../../assets/img/가지.png',
+      name: '가지 등급',
+      description: '성장과 확장의 상징으로, 공동체에 활력을 더하는 역할을 합니다.'
+    },
+    '나무': {
+      img: './../../assets/img/나무.png',
+      name: '나무 등급',
+      description: '튼튼하고 성실한 나무처럼 꾸준히 성장하는 등급입니다.'
+    },
+    '관리자': {
+      img: './../../assets/img/관리자.png',
+      name: '관리자 등급',
+      description: '커뮤니티를 관리하며 전체 운영을 책임지는 특별한 등급입니다.'
+    }
+  };
 
   // 커서 포인터 표시
   treeImages.forEach(img => {
