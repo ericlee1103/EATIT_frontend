daadf1067e6ec0640ac199ad214b8049816109ec
seojin12lee <aillsalovesophia@gmail.com>
2025-08-11 21:17:04 +0900

    250811 21:15 이서진 로그인페이지 경고메시지 넣는 js 진행중

--- webapp/assets/js/logIn/login.js
+++ webapp/assets/js/logIn/login.js
@@ -13,3 +13,42 @@ window.addEventListener('DOMContentLoaded', () => {
       document.getElementById('footer').innerHTML = data;
     });
 });
+
+let warning = document.getElementById('login_warning_message');
+let id = document.getElementById('login_input_id');
+let pw = document.getElementById('login_input_pw'); 
+let login = document.getElementById('login_btn');
+
+
+console.log(login);
+console.log(warning);
+console.log(id, pw);
+
+const db = {
+  id : 'user',
+  pw : 'useruser123',
+}
+
+login.addEventListener('click', () => {
+  // e.preventDefault();
+
+  if(id.value === db.id && pw.value === db.pw){
+    window.location.href = 'main.html';
+  }else{
+    // warning.style.display = 'block';
+    warning.textContent = '아이디 혹은 비밀번호를 다시 확인해주세요'
+    warning.style.color='red';
+    alert('아이디 혹은 비밀번호를 다시 확인해주세요');
+
+    pw.focus();
+    pw.select();
+  }
+});
+
+
+// btn[0].addEventListener('click', () => {
+//   if(!id || !pw){
+//   }
+//   location.replace('./../../app/findPW/editPw.html');
+//   alert('문자로 임시비밀번호가 발송되었습니다.')
+// });
