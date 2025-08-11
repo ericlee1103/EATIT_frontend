fd263ffbf4cd9a5ae3b01ea2a5e35565634adaa0
seojin12lee <aillsalovesophia@gmail.com>
2025-08-11 18:46:30 +0900

    250811 18:46 이서진 회원가입,로그인, 아이디비번 찾기 연결 1차 완성

--- webapp/assets/js/findId/findUserIdAuth.js
+++ webapp/assets/js/findId/findUserIdAuth.js
@@ -13,3 +13,22 @@ window.addEventListener('DOMContentLoaded', () => {
       document.getElementById('footer').innerHTML = data;
     });
 });
+
+const rep_btn = document.getElementsByClassName('findIdAuth_req_auth');
+
+rep_btn[0].addEventListener('click', () => {
+  // location.replace('./../../app/findId/editPw.html');
+  alert('문자로 인증번호가 발송되었습니다.')
+});
+
+const chk_rep_btn = document.getElementsByClassName('findIdAuth_chk_auth');
+const findId_btn = document.getElementsByClassName('findId_btn');
+
+chk_rep_btn[0].addEventListener('click', () => {
+  // 인증번호 검증
+});
+
+findId_btn[0].addEventListener('click', () => {
+  // 인증번호 확인 후 페이지 이동
+  location.replace('./../../app/findId/findUserIdSuccess.html');
+});
--- webapp/assets/js/findPW/findPw.js
+++ webapp/assets/js/findPW/findPw.js
@@ -13,3 +13,10 @@ window.addEventListener('DOMContentLoaded', () => {
       document.getElementById('footer').innerHTML = data;
     });
 });
+
+const btn = document.getElementsByClassName('findPw_btn');
+
+btn[0].addEventListener('click', () => {
+  location.replace('./../../app/findPW/editPw.html');
+  alert('문자로 임시비밀번호가 발송되었습니다.')
+});
